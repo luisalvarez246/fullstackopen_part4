5c1e4256cd689683e74c4dc4cf55a5325faff74a
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-11-18 11:33:25 +0100

    add middleware.js
    custom middleware unknownEndPoint & errorHandler

---
+++ utils/middleware.js
@@ -0,0 +1,19 @@
+const	unknownEndPoint = (request, response) =>
+{
+	response.status(404).send({error: 'unknown endpoint'});
+}
+
+const	errorHandler = (error, request, response, next) =>
+{
+	if (error.name === 'CastError')
+	{
+		response.status(400).send({ error: 'malformatted id' });
+	}
+	else if (error.name === 'ValidationError')
+	{
+		response.status(400).json({ error: error.message });
+	}
+	next(error);
+}
+
+module.exports = { unknownEndPoint, errorHandler };
