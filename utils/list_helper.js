e92ff75813699efdb94a4b8bd31d0994d3788d48
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-11-19 11:02:59 +0100

    add totalLikes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,4 +3,12 @@ const	dummy = (blogs) =>
 	return (1);
 }
 
-module.exports = { dummy };
+const	totalLikes = (blogs) =>
+{
+	let	likes;
+	
+	likes = blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0);
+	return (likes);
+}
+
+module.exports = { dummy, totalLikes };
