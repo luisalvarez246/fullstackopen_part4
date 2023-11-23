21b2d1ccd8f57424927fafbda5936cc7622994a1
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-11-23 11:40:12 +0100

    add POST requests tests

--- tests/testDB.test.js
+++ tests/testDB.test.js
@@ -5,6 +5,7 @@ const	Blog = require('../models/Blog');
 const	helper = require('./test_helper');
 
 const	api = supertest(app);
+const	url = '/api/blogs';
 
 beforeEach(async() =>
 {
@@ -28,7 +29,7 @@ describe('GET request testing', () =>
 		//arrange
 		let	response;
 		//act
-		response = await api.get('/api/blogs');
+		response = await api.get(url);
 		//assert
 		expect(response.status).toBe(200);
 		expect(response.header['content-type']).toMatch(/application\/json/);
@@ -40,7 +41,7 @@ describe('GET request testing', () =>
 		let	response;
 		let blogs;
 		//act
-		response = await api.get('/api/blogs');
+		response = await api.get(url);
 		blogs = response.body;
 		//assert
 		expect(blogs).toHaveLength(helper.initialBlogs.length);
@@ -52,10 +53,28 @@ describe('GET request testing', () =>
 		let	response;
 		let blogs;
 		//act
-		response = await api.get('/api/blogs');
+		response = await api.get(url);
 		blogs = response.body[0];
 		//assert
 		expect(blogs.id).toBeDefined();
+	})
+})
+
+describe('POST requests testing', () =>
+{
+	test('POST request creates a new blog', async () =>
+	{
+		//arrange
+		let	response;
+		let	blog;
+		let	blogsAtEnd;
+		//act
+		blog = helper.blogObject;
+		response = await api.post(url).send(blog);
+		blogsAtEnd = await helper.blogsInDb();
+		//assert
+		expect(response.status).toBe(200);
+		expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 	})
 })
 
