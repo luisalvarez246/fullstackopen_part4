0741cf18e9ea3eb4b0804c45137be285c8fcf92a
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-11-23 12:07:54 +0100

    add required to fields title and url

--- models/Blog.js
+++ models/Blog.js
@@ -3,9 +3,17 @@ const	mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema(
 {
-	title: String,
+	title: 
+	{
+		type: String,
+		required: true
+	},
 	author: String,
-	url: String,
+	url: 
+	{
+		type: String,
+		required: true
+	},
 	likes: 
 	{
 		type: Number,
