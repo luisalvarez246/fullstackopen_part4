38af50d2c8b5f9b36c4d2c56037ec7b67ef5a9a6
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-11-26 10:10:19 +0100

    add initialUsers, repeatedUsername

--- tests/users_test_helper.js
+++ tests/users_test_helper.js
@@ -1,8 +1,26 @@
 const	User = require('../models/User');
 
+const	initialUsers = 
+[
+	{
+		_id: '5a422a851b54a676234d17f7',
+		username: 'Finn',
+		password: 'jake',
+		name: 'Finn The Human',
+		__v: 0
+	},
+	{
+		_id: '5a422a851b54a676234d17f8',
+		username: 'Jake',
+		password: 'finn',
+		name: 'Jake The Dog',
+		__v: 0
+	}
+]
+
 const	invalidUsername =
 {
-	_id: '5a422a851b54a676234d17f7',
+	_id: '5a422a851b54a676234d17z7',
 	username: 'Fn',
 	password: 'jake',
 	name: 'Finn',
@@ -11,9 +29,18 @@ const	invalidUsername =
 
 const	missingUsername =
 {
-	_id: '5a422a851b54a676234d17f7',
+	_id: '5a422a851b54a676234d17z7',
 	password: 'jake',
 	name: 'Finn',
+	__v: 0
+}
+
+const	repeatedUsername =
+{
+	_id: '5a422a851b54a676234d17z8',
+	username: 'Jake',
+	password: 'finn',
+	name: 'Jake The Dog',
 	__v: 0
 }
 
@@ -25,4 +52,4 @@ const	usersInDb = async () =>
 }
 
 
-module.exports = { invalidUsername, missingUsername, usersInDb };
+module.exports = { initialUsers, invalidUsername, missingUsername, repeatedUsername, usersInDb };
