4f5096ab7204560cb02136c20c6f1e48ae9b5f10
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-11-26 11:22:35 +0100

    add blogs field

--- models/User.js
+++ models/User.js
@@ -12,7 +12,14 @@ const	userSchema = mongoose.Schema(
 		unique: true
 	},
 	passwordHash: String,
-	name: String
+	name: String,
+	blogs:
+	[
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Blog'
+		}
+	]
 })
 
 userSchema.set('toJSON',
