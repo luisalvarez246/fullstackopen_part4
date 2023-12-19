a5c7c9b188c3ef27a3f2dff504a8a6d2ece1e6fe
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-12-19 11:23:28 +0100

    add process to globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
 		'browser': true,
 		'commonjs': true,
 		'es2021': true,
-		'jest': true
+		'jest': true,
+		'process': true
 	},
 	'extends': 'eslint:recommended',
 	'overrides': [
